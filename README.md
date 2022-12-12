0f445277e89e37a194203277d4e538dd7424e1ce
Sourav Ganguli <tositglobal@gmail.com>
2022-12-12 06:22:51 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## License Type
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+
+
+# How to use 🤔
+
+## Installation
+
+Install fast-otp with npm
+
+```bash
+  npm i fast-otp
+```
+## Usage/Examples
+
+```javascript
+
+import any_name from 'fast-otp'
+
+console.log(any_name)
+```
+
+
+
---
+++ otp.js
@@ -0,0 +1,29 @@
+const otp = () => {
+    // Get the current time in milliseconds
+    const now = Date.now();
+
+    // Generate a random number based on the current time
+    const rand = Math.random() * now;
+
+    // Multiply the random number by 100000 to get a six-digit integer
+    const otp = Math.floor(rand * 100000);
+
+    // Convert the OTP to a string and log it
+    // console.log(otp.toString());
+
+    // Define a 12-digit number to convert
+    const num = otp.toString();
+
+    // Convert the number to a string and split it into an array of characters
+    const digits = num.toString().split('');
+
+    // Take the first six digits and combine them into a new string
+    const newNum = digits.slice(0, 6).join('');
+
+    // Convert the new string back to a number and log the result
+    const generator = parseInt(newNum, 10);
+    return generator;
+}
+
+const otpgen  =  otp()
+export default otpgen;
